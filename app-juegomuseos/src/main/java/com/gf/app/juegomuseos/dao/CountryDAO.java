9ba986bee939783ff75956fad2d8561084397357
WYMO <priparno@lab03-06.gregoriofer.local>
2023-05-20 18:53:18 +0200

    Terminados los DAOs de la base de datos (falta rankin)

--- app-juegomuseos/src/main/java/com/gf/app/juegomuseos/dao/CountryDAO.java
+++ app-juegomuseos/src/main/java/com/gf/app/juegomuseos/dao/CountryDAO.java
@@ -4,7 +4,6 @@
  */
 package com.gf.app.juegomuseos.dao;
 
-import com.gf.app.juegomuseos.models.Artwork;
 import com.gf.app.juegomuseos.models.Country;
 import com.gf.app.juegomuseos.utils.ConnectionDB;
 import java.sql.Connection;
--- app-juegomuseos/src/main/java/com/gf/app/juegomuseos/dao/MuseumDAO.java
+++ app-juegomuseos/src/main/java/com/gf/app/juegomuseos/dao/MuseumDAO.java
@@ -4,10 +4,52 @@
  */
 package com.gf.app.juegomuseos.dao;
 
+import com.gf.app.juegomuseos.models.Country;
+import com.gf.app.juegomuseos.models.Museum;
+import com.gf.app.juegomuseos.utils.ConnectionDB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author noelp
  */
 public class MuseumDAO {
-    
+
+    public List<Museum> selectAll() throws SQLException {
+        String sql = "SELECT * FROM museos";
+        List<Museum> lista = new ArrayList<>();
+        try ( Connection con = ConnectionDB.getConnection()) {
+            Statement s = con.prepareStatement(sql);
+            try ( ResultSet rs = s.executeQuery(sql)) {
+                while (rs.next()) {
+                    Museum m = new Museum();
+                    m.setId_museo(rs.getInt("id_museo"));
+                    m.setNombre_museo(rs.getString("nombre_museo"));
+                    m.setId_pais(rs.getInt("id_pais"));
+                }
+            }
+        }
+        return lista;
+    }
+
+    public Museum selectId(int id) throws SQLException {
+        String sql = "SELECT * FROM museos WHERE id_museo=" + id;
+        Museum m = new Museum();
+        try ( Connection con = ConnectionDB.getConnection()) {
+            Statement s = con.prepareStatement(sql);
+            try ( ResultSet rs = s.executeQuery(sql)) {
+                if (rs.next()) {
+                    m.setId_museo(rs.getInt("id_museo"));
+                    m.setNombre_museo(rs.getString("nombre_museo"));
+                    m.setId_pais(rs.getInt("id_pais"));
+                }
+            }
+        }
+        return m;
+    }
 }
