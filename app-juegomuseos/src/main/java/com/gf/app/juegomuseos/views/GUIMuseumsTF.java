99bedacce3e12aba7aa138f67f044d14a9cc5dbe
luisfc <luisfernandezlf.70@gmail.com>
2023-06-09 20:15:02 +0200

    Me cago en NetBeans 3 veces

--- app-juegomuseos/src/main/java/com/gf/app/juegomuseos/views/GUIMuseumsTF.java
+++ app-juegomuseos/src/main/java/com/gf/app/juegomuseos/views/GUIMuseumsTF.java
@@ -30,7 +30,7 @@ public class GUIMuseumsTF extends javax.swing.JFrame {
     /**
      * museumPanel: panel que contiene la etiqueta con el nombre del museo.
      */
-    private JPanel museumPanel;
+    private JPanel museumPanel;W
     
     /**
      * buttonsPanel: panel que contiene los dos botones con "Existe" y "No Existe"
