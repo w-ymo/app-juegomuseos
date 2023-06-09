bd99b2d6c5eed335e66e4aae451ae9134e4405ff
WYMO <priparno@lab03-06.gregoriofer.local>
2023-06-09 11:42:53 +0200

    No puedo mas 8

--- app-juegomuseos/src/main/java/com/gf/app/juegomuseos/utils/ImagesSize.java
+++ app-juegomuseos/src/main/java/com/gf/app/juegomuseos/utils/ImagesSize.java
@@ -18,7 +18,8 @@ public class ImagesSize {
 
     /**
      * getProportionalDimensionImage: devuelve una imagen que mantenga la
-     * relacion de aspecto dependiendo de la dimension pasada por parametro.
+     * relacion de aspecto dependiendo de la dimension pasada por parametro para
+     * que se vea de manera completa.
      *
      * @param i objeto de tipo {@link ImageIcon} con la imagen
      * @param gapSize objeto de tipo {@link Dimension} con las medidas donde se
@@ -29,7 +30,11 @@ public class ImagesSize {
         int x = i.getIconWidth();
         int y = i.getIconHeight();
         Dimension newDimension = new Dimension();
-        newDimension.setSize((int) (gapSize.getHeight() * x / y), gapSize.getHeight());
+        if (x > y) {
+            newDimension.setSize(gapSize.getWidth(), (int) (gapSize.getWidth() * y / x));
+        } else {
+            newDimension.setSize((int) (gapSize.getHeight() * x / y), gapSize.getHeight());
+        }
         return i.getImage().getScaledInstance(newDimension.width, newDimension.height, Image.SCALE_DEFAULT);
     }
 
