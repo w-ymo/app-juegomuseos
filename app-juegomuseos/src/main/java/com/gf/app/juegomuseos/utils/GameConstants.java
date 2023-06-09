a2cde980a643eb6a3cc392813e96199907dc8f66
LuisFC <luisfernandezlf.70@gmail.com>
2023-06-09 13:09:35 +0200

    Javadoc nosecuantos

--- app-juegomuseos/src/main/java/com/gf/app/juegomuseos/utils/GameConstants.java
+++ app-juegomuseos/src/main/java/com/gf/app/juegomuseos/utils/GameConstants.java
@@ -4,6 +4,12 @@
  */
 package com.gf.app.juegomuseos.utils;
 
+import com.gf.app.juegomuseos.controller.MainController;
+import com.gf.app.juegomuseos.controller.RankingController;
+import com.gf.app.juegomuseos.controller.SelectGameController;
+import com.gf.app.juegomuseos.views.GUIPrincipal;
+import com.gf.app.juegomuseos.views.GUIRanking;
+import com.gf.app.juegomuseos.views.GUISelectGame;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -37,22 +43,42 @@ public interface GameConstants {
     /**
      * MAIN_MENU_OPTIONS: es un array de cadenas de caracteres que indica las 
      * opciones presentadas en la ventana principal de la aplicacion.
+     * 
+     * @see GUIPrincipal
+     * @see MainController
      */
     public static final String[] MAIN_MENU_OPTIONS = {"MODO YINCANA", "MODO LIBRE", "RANKING", "INFORMACION", "AJUSTES", "SALIR"};
 
     /**
      * GAMES_NAMES: es un array de cadenas de caracteres que indica los juegos 
      * disponibles en la ventana del modo libre de la aplicacion.
+     * 
+     * @see GUISelectGame
+     * @see SelectGameController
      */
     public static final String[] GAMES_NAMES = {"¿Quién lo hizo?", "Verdadero/Falso de museos", "Gregorio Fernández", "Coloca en el mapa"};
 
     /**
-     * GAMES_CODES
+     * GAMES_CODES: es un array de cadenas de caracteres que contiene el codigo
+     * de los juegos.
+     * 
+     * @see GUISelectGame
+     * @see SelectGameController
      */
     public static final String[] GAMES_CODES = {WHOIS_CODE, TRUEFALSE_CODE, GREGORIO_CODE, MAPGAME_CODE};
 
+    /**
+     * SCREEN_SIZE: es la dimension de tipo {@link Dimension} de la pantalla.
+     */
     public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 
+    /**
+     * RANKING_COLUMNS: es un array de cadenas de caracteres que contriene los 
+     * nombres de las columnas del ranking.
+     * 
+     * @see GUIRanking
+     * @see RankingController
+     */
     public static final String[] RANKING_COLUMNS = {"Nombre", "Puntuación"};
 
 }
